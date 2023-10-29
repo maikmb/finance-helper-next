08d04bef6519db32cf2fd19c81ce721074e4d88d
Maik Braga Moura <65735194+maikvortx@users.noreply.github.com>
2023-10-28 21:29:58 -0300

    basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'build'
+    distDir: 'build',
+    basePath: '/finance-helper-next'
 }
 
 module.exports = nextConfig
